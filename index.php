6a585ad01be9817a360da4bb3160b19cb9da2527
Eric <e.albrecht@laposte.net>
2020-01-08 21:52:52 +0100

    Ajout mise en forme

--- index.php
+++ index.php
@@ -1,42 +1,44 @@
 <?php
+//******************** PHP - chargement des donnees ****************/
+
 // recuperation du numero de page, si aucun parametre, on charge la page 1.
-(isset($_GET['id'])) ? $id = $_GET['id']:$id = 1;
+(isset($_GET['id'])) ? $id = $_GET['id']:$id = 0;
 
 // on passe la variable get en integrale
-	$id = (int)$id;
+$id = (int)$id; //numero de page, !!!!!!! a securiser
 // initialisation des variables
-	 $title='';
-	 $menu='';
-	 $content='';
-
+$title='';
+$menu='';
+$content='';
+$nav =''; 
 // recup du fichier xml
-if (!file_exists('source.xml')) {
-	 exit('Impossible de lire individus.xml.');
-}else{
-//on charge le fichier xml	
-$content_page = simplexml_load_file('source.xml');
+if (!file_exists('source.xml')) 
+	{
+		exit('Impossible de lire source.xml !!.');
+	}
+else
+	{
+		//on charge le fichier xml	
+		$content_page = simplexml_load_file('source.xml');
 
-// integration des valeurs
-$title = $content_page->page['id'];
-
-$title = $content_page->page[$id]->title;
-$menu = $content_page->page[$id]->menu;
-$content = $content_page->page[$id]->content;
-
-$nav =""; 
-//echo $title;
-$i=0;
-foreach ($content_page as $page) {
-//echo $page->menu;
-$nav = $nav.'<li><a href="?id='.$i.'">'.$page->menu.'</a></li>';
-$i++;
-}
-
-}
+		// integration des valeurs dans les variables en fonction de la page demandée
+		$title = $content_page->page['id'];
+		$title = $content_page->page[$id]->title;
+		$menu = $content_page->page[$id]->menu;
+		$content = $content_page->page[$id]->content;
 
 
+		// recuperation des menus pour creer un la navBar
+		$i=0; // on compte les pages
+		foreach ($content_page as $page) {
+		//echo $page->menu;
+		$nav = $nav.'<li class="nav-item"><a class="nav-link active" href="?id='.$i.'">'.$page->menu.'</a></li>';
+		$i++;
+		}
+
+	}
 ?>
-
+<!-- ******************************** HTML ****************************************-->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -44,15 +46,25 @@ $i++;
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="https://www.fontify.me/wf/822845a211e250d3989a107203e72537" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+	
+	<link rel="stylesheet" href="assets/css/style.css">
 	<title><?= $title; ?></title>
 </head>
 <body>
-	
-	<div class="content">
-	<ul>
-		<?= $nav; ?>
-	</ul>
-	<?= $content; ?></div>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+			<ul class="nav">
+				<?= $nav; ?>
+			</ul>
+		</nav>
+	<div class="container jumbotron">
+		<div class="row ">
+			<div class="col-12">
+				<?= $content; ?>
+			</div>	
+		</div>	
+	</div>
 </body>
 </html>
 
