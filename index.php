020f2fc95d7440ab38a8d5cd147e50441dca38ef
Eric <e.albrecht@laposte.net>
2020-01-08 22:20:44 +0100

    ajout de plusieurs commentaires

--- index.php
+++ index.php
@@ -1,42 +1,56 @@
 <?php
 //******************** PHP - chargement des donnees ****************/
 
-// recuperation du numero de page, si aucun parametre, on charge la page 1.
-(isset($_GET['id'])) ? $id = $_GET['id']:$id = 0;
-
-// on passe la variable get en integrale
-$id = (int)$id; //numero de page, !!!!!!! a securiser
-// initialisation des variables
-$title='';
-$menu='';
-$content='';
-$nav =''; 
-// recup du fichier xml
-if (!file_exists('source.xml')) 
-	{
-		exit('Impossible de lire source.xml !!.');
-	}
-else
-	{
-		//on charge le fichier xml	
-		$content_page = simplexml_load_file('source.xml');
-
-		// integration des valeurs dans les variables en fonction de la page demandée
-		$title = $content_page->page['id'];
-		$title = $content_page->page[$id]->title;
-		$menu = $content_page->page[$id]->menu;
-		$content = $content_page->page[$id]->content;
+// recuperation du numero de page, si aucun parametre, on charge la page 1 qui a l'id 0.
+((isset($_GET['id']))&&  is_numeric($_GET['id'])) ? $id = $_GET['id']:$id = 0;
 
 
-		// recuperation des menus pour creer un la navBar
-		$i=0; // on compte les pages
-		foreach ($content_page as $page) {
-		//echo $page->menu;
-		$nav = $nav.'<li class="nav-item"><a class="nav-link active" href="?id='.$i.'">'.$page->menu.'</a></li>';
-		$i++;
+function XmlToHtml($id){
+		/***************************************************************************
+		 * cette fonction collecte les données depuis un fichier xml,puis les tris.*
+		 * Elle retourne les données correspondantes à une page donnée.            *
+		 ***************************************************************************/
+
+		// on convertit la variable obtenu par _GET en integrale
+		$id = (int)$id; //numero de page
+		// initialisation des variables
+		$title='';
+		$menu='';
+		$content='';
+		$nav =''; 
+		
+		// recup du fichier xml
+		if (!file_exists('source.xml')) 
+			{
+				exit('Impossible de lire source.xml !!.');
+			}
+		else
+			{
+				//on charge le fichier xml	
+				$content_page = simplexml_load_file('source.xml');
+
+				// integration des valeurs dans les variables en fonction de la page demandée
+				//$title = $content_page->page['id'];
+				$title = $content_page->page[$id]->title;
+				$menu = $content_page->page[$id]->menu;
+				$content = $content_page->page[$id]->content;
+
+
+				// recuperation des menus pour creer un la navBar
+				$i=0; // on compte les pages
+				foreach ($content_page as $page) {
+				//echo $page->menu;
+				$nav = $nav.'<li class="nav-item"><a class="nav-link active" href="?id='.$i.'">'.$page->menu.'</a></li>';
+				$i++;
+				
+				
+				}
+
+			}
+			$data = ['title'=>$title,'nav'=>$nav,'content'=>$content];
+			return $data;			
 		}
-
-	}
+$data = XmlToHtml($id);
 ?>
 <!-- ******************************** HTML ****************************************-->
 
@@ -50,18 +64,18 @@ else
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 	
 	<link rel="stylesheet" href="assets/css/style.css">
-	<title><?= $title; ?></title>
+	<title><?=  $data['title']; ?></title>
 </head>
 <body>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
 			<ul class="nav">
-				<?= $nav; ?>
+				<?= $data['nav']; ?>
 			</ul>
 		</nav>
 	<div class="container jumbotron">
 		<div class="row ">
 			<div class="col-12">
-				<?= $content; ?>
+				<?= $data['content']; ?>
 			</div>	
 		</div>	
 	</div>
